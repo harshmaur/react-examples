4dba2fe2f66fc83535ffe3581516cf896d4a31d6
Harsh Maur <harsh@comparemunafa.com>
2016-04-25 18:50:34 +0530

    copied booklist to weather

---
+++ weather/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title : "Javascript: The good parts", pages: 101},
+        { title : "Harry Potter", pages: 32},
+        { title : "The Dark Tower", pages: 1},
+        { title : "Divergent", pages: 1000}
+    ]
+}
