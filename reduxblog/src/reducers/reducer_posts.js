98d4e88810b10f121c6ec7d6656e3ee627fe1920
Harsh Maur <harsh@comparemunafa.com>
2016-04-28 15:36:34 +0530

    finished blog

--- reduxblog/src/reducers/reducer_posts.js
+++ reduxblog/src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions/index'
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index'
 
 
 
@@ -6,8 +6,12 @@ const INITIAL_STATE = { all: [], post: null }
 
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
+        case FETCH_POST:
+            return {...state, post: action.payload.data}
         case FETCH_POSTS:
             return {...state, all: action.payload.data}
+        case DELETE_POST:
+            return {...state}
         default:
             return state
     }
